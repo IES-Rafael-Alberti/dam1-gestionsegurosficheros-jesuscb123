cb97b5d2fb8ad338144a67e957500fd2c6459055
jesuscb123 <cbjesus@iesrafaelalberti.es>
2025-03-28 11:45:07 +0100

    Gestor Usuarios finalizado.

---
+++ src/main/kotlin/service/GestorUsuarios.kt
@@ -0,0 +1,43 @@
+package prog2425.dam1.seguros.service
+
+import prog2425.dam1.seguros.data.IRepoUsuarios
+import prog2425.dam1.seguros.model.Perfil
+import prog2425.dam1.seguros.model.Usuario
+import prog2425.dam1.seguros.utils.IUtilSeguridad
+
+class GestorUsuarios(val repoUsuarios: IRepoUsuarios, val seguridad: IUtilSeguridad) : IServUsuarios {
+    override fun iniciarSesion(nombre: String, clave: String): Perfil? {
+       val usuarioExiste = buscarUsuario(nombre)
+        if (usuarioExiste != null && usuarioExiste.clave == clave) return usuarioExiste.perfil else return null
+    }
+
+    override fun agregarUsuario(nombre: String, clave: String, perfil: Perfil): Boolean {
+        if (buscarUsuario(nombre) == null){
+            val claveEncriptada = seguridad.encriptarClave(clave)
+          return repoUsuarios.agregar(Usuario(nombre,claveEncriptada,perfil))
+        }
+        return false
+    }
+
+    override fun eliminarUsuario(nombre: String): Boolean {
+        if (buscarUsuario(nombre) != null) return repoUsuarios.eliminar(nombre) else return false
+    }
+
+    override fun cambiarClave(usuario: Usuario, nuevaClave: String): Boolean {
+        val nuevaClaveEncriptada = seguridad.encriptarClave(nuevaClave)
+        if (repoUsuarios.cambiarClave(usuario, nuevaClaveEncriptada)) return true else return false
+    }
+
+    override fun buscarUsuario(nombre: String): Usuario? {
+        val usuarioExiste = repoUsuarios.buscar(nombre)
+        if (usuarioExiste != null) return usuarioExiste else return null
+    }
+
+    override fun consultarTodos(): List<Usuario> {
+        return repoUsuarios.obtenerTodos()
+    }
+
+    override fun consultarPorPerfil(perfil: Perfil): List<Usuario> {
+        return consultarTodos().filter { it.perfil == perfil }
+    }
+}
--- src/main/kotlin/utils/Seguridad.kt
+++ src/main/kotlin/utils/Seguridad.kt
@@ -1,7 +1,7 @@
 package prog2425.dam1.seguros.utils
 import at.favre.lib.crypto.bcrypt.BCrypt
 
-class Seguridad : IUtilSeguridad {
+object Seguridad : IUtilSeguridad {
     override fun encriptarClave(clave: String, nivelSeguridad: Int): String {
         return BCrypt.withDefaults().hashToString(nivelSeguridad, clave.toCharArray())
     }
